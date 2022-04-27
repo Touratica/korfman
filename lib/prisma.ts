6f415730306b8f5d41eda5d676fdc902a50ce7af
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2022-04-27 16:13:49 +0100

    Implement global instance of prisma on development

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,15 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
 
-export default prisma;
+export default prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
