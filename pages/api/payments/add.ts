c70cd54ddb132a7d6303ef18e0673b71e33f427a
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2022-05-03 15:14:23 +0100

    Add HTTP method verification

--- pages/api/payments/add.ts
+++ pages/api/payments/add.ts
@@ -1,13 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../lib/prisma';
 
-const handle = async (req:NextApiRequest, res:NextApiResponse) => {
-	const result = await prisma.payment.create({
-		data: {
-			...req.body,
-		},
-	});
-	res.status(200).json(result);
+const handle = async (req:NextApiRequest, res:NextApiResponse) => {	
+	switch (req.method) {
+		case 'POST':
+			await handlePOST(req, res);		
+			break;
+		default:
+			throw new Error(`The HTTP ${req.method} method is not supported at this route.`);
+	};
+	
+	await handlePOST(req, res);
 };
+
+async function handlePOST(req: NextApiRequest, res: NextApiResponse<any>) {
+	try {
+		const result = await prisma.payment.create({
+			data: {
+				...req.body,
+			},
+		});
+		res.status(200).json(result);
+	} catch (error) {
+		res.status(418).json({ message: `Could not create the payment.` });
+	}
+}
 
 export default handle;
