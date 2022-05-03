6b096fb465bf0d6c374def03ab7d7d028423f410
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2022-05-03 15:17:37 +0100

    Add HTTP method verification on fpcMember adding

--- pages/api/fpcMembers/add.ts
+++ pages/api/fpcMembers/add.ts
@@ -2,12 +2,22 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../lib/prisma';
 
 const handle = async (req:NextApiRequest, res:NextApiResponse) => {
+	switch (req.method) {
+		case 'POST':
+			await handlePOST(req, res);
+			break;
+		default:
+			throw new Error(`The HTTP ${req.method} method is not supported at this route.`);
+	};
+};
+
+export default handle;
+
+async function handlePOST(req: NextApiRequest, res: NextApiResponse<any>) {
 	const result = await prisma.fpcMember.create({
 		data: {
 			...req.body,
 		},
 	});
 	res.status(200).json(result);
-};
-
-export default handle;
+}
