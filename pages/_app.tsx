2cada47cbb9ad104e550d7628fc76ec376af132d
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2022-06-21 15:09:16 +0100

    Move to Tailwind CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,8 @@
-import { GetServerSidePropsContext } from 'next';
-import { useState } from 'react';
-import { AppProps } from 'next/app';
-import { getCookie, setCookies } from 'cookies-next';
-import Head from 'next/head';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
-export default function App(props: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Mantine next example</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="shortcut icon" href="/favicon.svg" />
-      </Head>
-    </>
-  );
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
-  colorScheme: getCookie('mantine-color-scheme', ctx) || 'light',
-});
+export default MyApp;
